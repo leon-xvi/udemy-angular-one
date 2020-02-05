3599a9075ffb8f7138c07ee6c02ce2f12ed723a7
Justin Walker <walkerjt93@gmail.com>
2020-02-05 15:06:05 -0500

    - removed package-lock.json and added yarn.lock because i'm using yarn now instead of npm - added an input to the servers component, and binding the input event to a method on the module called onUpdateServerName(event) - also bound the serverName value in the module to the value of the event target, and also using string interpolation to display it - this goes through the completion of section 2 - 30

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serverCreationStatus = 'No server was created!'
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
   constructor() {
-    setTimeout(() => {this.allowNewServer = true}, 2000)
+    setTimeout(() => {this.allowNewServer = true}, 2000);
    }
 
   ngOnInit() {
@@ -21,4 +22,7 @@ export class ServersComponent implements OnInit {
     this.serverCreationStatus = 'Server was created';
   }
 
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
