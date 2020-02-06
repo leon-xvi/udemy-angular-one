8432456eb0c6d85855bad66e1d04d4aa2f2aac51
Justin Walker <justin.walker@webteks.com>
2020-02-05 19:19:05 -0500

    - bound to the ngStyle directive to set style property of an element,   and basically within that data binding created a function that checks if a server is online or offline, and uses a ternary operator to decide whether to style it green or red

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 
 export class ServerComponent {
   serverId: number = 10;
-  serverStatus: string = 'offline'
+  serverStatus: string = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
-    return this.serverStatus
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
